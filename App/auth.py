052c20dea5299a09a84b3565f06ff92decfe5899
LaurenzSoltau <laurenz.soltau@icloud.com>
2022-11-16 20:56:40 +0100

    basic structure and register view

---
+++ App/auth.py
@@ -0,0 +1,48 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from App.db import get_db
+
+# create blueprint named auth and the urls prefix auth
+bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+# view to register a new user
+@bp.route("/register", methods=["GET", "POST"])
+def register():
+    # if method is Post check inputs and load into database
+    if request.method == "POST":
+        # get the data from the form
+        username = request.form["usedrname"]
+        password = request.form["password"]
+        db = get_db() # load database
+        error = None
+
+        # guard clauses to validate inputs
+        if not username:
+            error = "Username required."
+        elif not password:
+            error = "Password required."
+
+        # if the input is valid call database and save the new user in users
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)", 
+                    (username, generate_password_hash(password)),
+                )
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+        
+            else:
+                return redirect(url_for("auth.login"))
+        
+        flash(error)
+
+
+    return render_template("auth/register.html")
+
+
---
+++ App/schema.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS post;
+
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    subjects TEXT NOT NULL,
+    grade_from INTEGER NOT NULL, 
+    grade_to Integer NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE post (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    offerer INTEGER NOT NULL,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    subject TEXT NOT NULL,
+    grade_from TEXT NOT NULL,
+    grade_to TEXT NOT NULL,
+    FOREIGN KEY (offerer) REFERENCES user (id)
+);
